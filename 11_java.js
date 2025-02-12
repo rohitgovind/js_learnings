c738b87d8db9b339c3a19fde51b85d93f08596a4
rohitgovind <112809766+rohitgovind@users.noreply.github.com>
2025-02-12 09:43:12 +0530

    Add files via upload

---
+++ 11_java.js
@@ -0,0 +1,11 @@
+// const newObject = {
+//     name: "Rohit",
+//     fullname: {
+//         firstname: {
+//             userFirstname: "Rao",
+//         }
+//     }
+// }
+
+// console.log(newObject.fullname.firstname.userFirstname)
+
