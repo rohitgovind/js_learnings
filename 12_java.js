f72d580d2680e8ff289f2e1d7ada401120aa76e5
rohitgovind <112809766+rohitgovind@users.noreply.github.com>
2025-02-12 09:43:26 +0530

    Add files via upload

---
+++ 12_java.js
@@ -0,0 +1,15 @@
+//destructure of array
+const course = {
+    coursename : "js in hindi",
+    price: "999",
+    courseInstructor: "Ram",
+}
+
+const {courseInstructor} = course
+console.log(courseInstructor)
+
+// {
+//     'name': "hitesh",
+//     'coursename': "js in hindi",
+//     'price' : "free",
+// }
