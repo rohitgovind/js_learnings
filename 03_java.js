2393d34e2a0d2a9390be63c4c3eb07a602a22876
rohitgovind <112809766+rohitgovind@users.noreply.github.com>
2025-02-12 09:40:35 +0530

    Add files via upload
    added 03_java

---
+++ 03_java.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// alert(3 + 3)
+
+var b =3
+const c = 3
+ 
+
+// b = 10
+//c = 10
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+let a = 10
+
+if( a == 10){
+    a = 20
+    console.log(a)
+}
+
+console.log(a)
