fc7c3312553f0e43fc7dc90c82ae426a6b2a33be
rohitgovind <112809766+rohitgovind@users.noreply.github.com>
2025-02-12 09:44:20 +0530

    Add files via upload

---
+++ 15_java.js
@@ -0,0 +1,13 @@
+// Global execution cotext 
+// function execution context
+
+// -> memory allocation phase
+// -> execution phase 
+
+
+
+// -------------------------------------
+
+// learn this again 
+
+// -------------------------------------
