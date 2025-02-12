b7b32fa10e348ed466f03c962926a51489beb34a
rohitgovind <112809766+rohitgovind@users.noreply.github.com>
2025-02-12 09:45:01 +0530

    Add files via upload

---
+++ 17_java.js
@@ -0,0 +1,13 @@
+// array = [1,2,3,4,5]
+// for (let i = 0; i < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 0; j <= 10; j++){
+//         console.log(`inner loop value: ${j} and outer loop value ${i}`);     
+//     }
+// }
