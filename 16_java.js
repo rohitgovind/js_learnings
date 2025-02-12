252e52390fa1d31177d585456048d124375de4a6
rohitgovind <112809766+rohitgovind@users.noreply.github.com>
2025-02-12 09:44:43 +0530

    Add files via upload

---
+++ 16_java.js
@@ -0,0 +1,53 @@
+// control flow
+
+// if, while and for loops
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month){
+    case 1:
+        console.log(`january`);
+        break;
+    case 2:
+        console.log(`february`);
+        break;
+    case 3:
+        console.log(`march`);
+        break;
+    case 4:
+        console.log(`april`);
+        break;
+    case 5:
+        console.log(`may`);
+        break;
+}
+
+
+// falsy values
+// false
+// 0
+// -0
+// BigInt
+// 0n
+// ""
+// null
+// undefined
+// NaN
+
+// truthy value 
+// "0"
+// 'false'
+// " "
+// []
+// {}
+// function(){} 
